ed73d2a6e7cca726dc8a66f054cb999875ea235a
Ness Hm <nessma.hamidou@insat.u-carthage.tn>
2022-05-05 16:09:02 +0100

    first commit

---
+++ src/Controller/ExamenController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExamenController extends AbstractController
+{
+    #[Route('/examen', name: 'app_examen')]
+    public function index(): Response
+    {
+        return $this->render('examen/index.html.twig', [
+            'controller_name' => 'ExamenController',
+        ]);
+    }
+}
